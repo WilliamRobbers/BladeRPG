0336e40fadce23584e7633612e0d82c355c48abf
WilliamRobbers <69828637+WilliamRobbers@users.noreply.github.com>
2020-08-18 14:06:25 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import random
+print("Hello, Welcome to BladeRPG!\n")
+print("Available classes: (Warrior, Knight, Wizard, Medic)")
+PlrClasses = input("Choose 2 classes (seperated by a space): ").split()
+classes = {}
+classes["plr1"] = [PlrClasses[0]]
+classes["plr2"] = [PlrClasses[1]]
+
+print(classes)
+
+#Warrior - Dual Tomohawks, Rush Ability(+DMG, -DEF)
+#Knight - Claymore, Sturdy Shield(+DEF, -DMG)
+#Wizard - Staff, Blast(DMG to all opponents. 3 round cooldown)
+#Medic - Bandages, Heal Surge(Heals entire team. 3 round cooldown)
